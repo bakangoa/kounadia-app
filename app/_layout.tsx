7c7f911961ba150730bae4e8b565e5621567ec15
Nanok <konancharlesyao@gmail.com>
2025-07-10 22:45:02 +0000

    refact: gluestack installed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
 import "@/src/di/register";
 import { store } from "@/src/store";
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
 
 export default function RootLayout() {
-  return <Provider store={store}>
-    <Stack />
-  </Provider>;
+  return (
+    <GluestackUIProvider mode="light">
+      <Provider store={store}>
+        <Stack />
+      </Provider>
+    </GluestackUIProvider>
+  );
 }
