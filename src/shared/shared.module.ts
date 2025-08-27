b60e8dabdd107a2648b4e0e00d39d15e1b6e1233
Nanok <konancharlesyao@gmail.com>
2025-08-27 07:22:40 +0000

    feat: notification toasr added

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,11 +3,15 @@ import { listenerMiddleware } from "../store/listener";
 import { Module } from "./core/module";
 import { ListenerActionHandler } from "./redux/actions-handler";
 import { NavigationHandler } from "./redux/navigation/navigation.handler";
+import { NotifierHandler } from "./redux/notifier/notifier.handler";
+import { StringUtils } from "./utils/string.utils";
 
 const TOKENs = {
     ListenerMiddleware: Symbol('ListenerMiddleware'),
     ActionHandler: Symbol('ActionHandler'),
-    Navigation: Symbol('Navigation')
+    Navigation: Symbol('Navigation'),
+    Notifier: Symbol('Notifier'),
+    StringUtils: Symbol('StringUtils')
 }
 
 export const SharedModule: Module = {
@@ -16,8 +20,11 @@ export const SharedModule: Module = {
         container.register(TOKENs.ListenerMiddleware, () => listenerMiddleware);
         container.register(TOKENs.ActionHandler, () => new ListenerActionHandler(container.resolve(TOKENs.ListenerMiddleware)));
         container.register(TOKENs.Navigation, () => new NavigationHandler(container.resolve(TOKENs.ActionHandler)));
+        container.register(TOKENs.Notifier, () => new NotifierHandler(container.resolve(TOKENs.ActionHandler)));
+        container.register(TOKENs.StringUtils, () => new StringUtils());
     },
     initialize: () => {
         (container.resolve(TOKENs.Navigation) as NavigationHandler).handle();
+        (container.resolve(TOKENs.Notifier) as NotifierHandler).handle();
     }
 }
